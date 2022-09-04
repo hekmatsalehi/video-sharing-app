3561f7c25d85229ab4e6268e4d5f7c23ccd166c3
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-09-04 13:11:06 -0700

    Designed the left side menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
 
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Container>
+      <Menu/>
+      <Main>
+        <Navbar/>
+        <Wrapper>
+          Video card
+        </Wrapper>
+      </Main>
+    </Container>
   );
 }
 
